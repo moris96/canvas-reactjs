f6304ecb5a15ade5df74739518699a18527afdee
moris96 <moriskhoudari@gmail.com>
2023-07-07 10:18:04 -0400

    animated circle

--- src/Canvass.js
+++ src/Canvass.js
@@ -1,19 +1,36 @@
-import { Canvas } from "canvas";
+// import { Canvas } from "canvas";
 import { useRef, useEffect } from "react";
 
 export default function Canvass(props){
     const canvasRef = useRef(null)
-    // const canvas = canvasRef.current
-    // const context = canvas.getContext('2d')
-
-    useEffect(() => {
-        const canvas = canvasRef.current
-        const context = canvas.getContext('2d')
-        //first draw
-        context.fillstyle = '#000000'
-        context.fillRect(0, 0, context.canvas.width, context.canvas.height)
-    }, [])
+  
+    const draw = (ctx, frameCount) => {
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+      ctx.fillStyle = '#000000'
+      ctx.beginPath()
+      ctx.arc(50, 100, 20*Math.sin(frameCount*0.01)**2, 0, 2*Math.PI)
+      ctx.fill()
+    }
     
-
+    useEffect(() => {
+      
+      const canvas = canvasRef.current
+      const context = canvas.getContext('2d')
+      let frameCount = 0
+      let animationFrameId
+      
+      //Our draw came here
+      const render = () => {
+        frameCount++
+        draw(context, frameCount)
+        animationFrameId = window.requestAnimationFrame(render)
+      }
+      render()
+      
+      return () => {
+        window.cancelAnimationFrame(animationFrameId)
+      }
+    }, [draw])
+    
     return <canvas ref={canvasRef} {...props}/>
 };
